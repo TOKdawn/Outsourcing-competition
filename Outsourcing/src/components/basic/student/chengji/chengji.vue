946d05bc50672b4b7ddf636bea4bfa008ad02334
TOKdawn <dawninshadow0.0@gmail.com>
2018-03-29 21:35:48 +0800

    ...

--- Outsourcing/src/components/basic/student/chengji/chengji.vue
+++ Outsourcing/src/components/basic/student/chengji/chengji.vue
@@ -1,19 +1,59 @@
 <template>
- 
-      <LineExample></LineExample>
+     <div>
+       
+              <Card class="chengji">
+                <p slot="title">C语言提升</p>
+                  <p>考试时间2018-5-28</p>
+            <div>89</div>
+                <p class="chengji_tea">查看老师评价</p>
+            </Card>
+                   <Card class="chengji">
+                <p slot="title">JavaScript高级程序设计</p>
+                  <p>考试时间2018-5-28</p>
+                <div>89</div>
+                  <p class="chengji_tea">查看老师评价</p>
+            </Card>
+                 
+                   <Card class="chengji">
+                <p slot="title">深入浅出设计模式</p>
+                <p>考试时间2018-5-28</p>
+               <div>68</div>
+                <p class="chengji_tea">查看老师评价</p>
+            </Card>
+              <Card class="chengji">
+                <p slot="title">PHP应用开发</p>
+                  <p>考试时间2018-5-28</p>
+               <div>79</div>
+                  <p class="chengji_tea">查看老师评价</p>
+            </Card>
+              <Card class="chengji">
+                <p slot="title">Python程序设计</p>
+                 <p>考试时间2018-5-28</p>
+                <div>89</div>
+                <p class="chengji_tea">查看老师评价</p>
+            </Card>
+     </div>
 </template>
 <script>
-   import LineExample from './chart.js'
-    export default {
-     
-      components: {
-        LineExample
-      }
-    }
 
-
-
+export default {
+}
 </script>
 <style scoped>
-
+.chengji{
+  width: 250px;
+  display: inline-block;
+  margin: 25px 15px;
+}
+.chengji_tea{
+  text-align: right ;
+}
+.chengji div{
+  font-size: 38px;
+  font-weight: 700;
+  text-align: center;
+  margin: 30px;
+}
 </style>
+
+
